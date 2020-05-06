eeebcfa83eba5b1d336692437b678a66aa0520d8
mattelmore83 <github@mattelmore.com>
2020-05-05 19:37:37 -0500

    a

---
+++ Day1/class-stack-order.py
@@ -0,0 +1,18 @@
+# refer to snippets2.pptx
+def oh_man():
+    a = 20
+    b = 30
+    c = 40
+
+def say_hi():
+    print('Hello')
+    a = 5
+    b = 6
+    oh_man()
+    c = a + b
+    print(c)
+
+say_hi()
+print('>>> Done')
+
+# Variables are local per stack
