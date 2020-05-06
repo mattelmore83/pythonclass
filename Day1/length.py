994e362df04f568e296bf6a1fa54f20dc1793c04
mattelmore83 <github@mattelmore.com>
2020-05-06 10:00:15 -0500

    commit

--- Day1/length.py
+++ Day1/length.py
@@ -11,8 +11,6 @@ l = (10 / 3.281) # length of garage converted to meters
 s = (12 / 3.281) # length of car / stationary length converted to meters
 c = (2.997925 * 10**8) # speed of light constant
 
-# formula = l=sl*(sqrt(1-(v**2)/(2.998*(10**8))))
-
 # formula changed to solve for v
 # use wolframalpha hints to step through this solution
 # shortcut link https://bit.ly/3djVdn6
